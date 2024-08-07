9bda1f92aa0841481f2b84844aac9194917ec2ed
MaralSI <maral.sagynalieva@gmail.com>
2024-08-07 09:11:09 +0600

    'lesson2'

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post (models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+def _str_(self):
+    return self.title
+
--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,7 @@ import random
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse(f"Hello World {random.randint(a: 0, b: 1000)}")
+    return HttpResponse(f"Hello World {random.randint(0, 1000)}") # type: ignore
 
 def main_view(request):
     return render(request,  "main.html")
